54bf9e37f1ccde750b41b847275583b711af9549
LaurisHebert <lrshebert@gmail.com>
2023-06-06 16:38:58 +0200

    Ajout d'une entrée

---
+++ assets/js/addTask.js
@@ -0,0 +1,23 @@
+let d = document;
+let addLine = d.querySelector("#addLine");
+let addBtn = d.querySelector("#addBtn");
+
+
+let oldStorage = JSON.parse(localStorage.getItem("task"));
+
+let newStorage = [];
+
+if (oldStorage) {
+    for (let i = 0; i < oldStorage.length; i++) {
+        newStorage.push(oldStorage[i]);
+    }
+}
+addBtn.addEventListener("click",  e=>{
+
+    let task = {
+        Text: addLine.value,
+        statement: "undo"
+    }
+    newStorage.push(task);
+    localStorage.setItem("task", JSON.stringify(newStorage));
+});
