ac8fa4dea668fe101f46254ff7e6aa28e6657105
LaurisHebert <lrshebert@gmail.com>
2023-06-12 13:39:07 +0200

    Fin du néttoyage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,85 @@
-let d = document;
-let addLine = d.querySelector("#addLine");
+const d = document;
+const addLine = d.querySelector("#addLine");
 
-let btnAdd = d.querySelector("#btnAdd");
-let btnCancel = d.querySelector("#btnCancel");
-let btnUpdateTitle = d.querySelector("#btnUpdateTitle");
+const btnUpdateTitle = d.querySelector("#btnUpdateTitle");
+const btnAdd = d.querySelector("#btnAdd");
+const btnCancel = d.querySelector("#btnCancel");
 
-let aFaire = d.querySelector("#aFaire");
-let fait = d.querySelector("#fait");
+const aFaire = d.querySelector("#aFaire");
+const fait = d.querySelector("#fait");
+
+let tasks;
 
 // récupération de l'ancien localStorage
-let tasks = [];
-tasks.push(JSON.parse(localStorage.getItem("task")));
-console.log(tasks)
+if (localStorage.getItem("task") !== null) {
+    tasks = JSON.parse(localStorage.getItem("task"));
+}else {
+    tasks = [];
+}
 
+displayTasks()
 
-showTasks()
+function putInLocalStorage() {
+    localStorage.setItem("task", JSON.stringify(tasks));
+    displayTasks()
+}
 
-
-btnAdd.addEventListener("click", function (event) {
+btnAdd.addEventListener("click", e=>{
+    console.log(addLine.value)
     let task = {
-        Text: addLine.value, statement: "undo"
+        Text: addLine.value,
+        statement: "undo"
     }
     tasks.push(task);
-    localStorage.setItem("task", JSON.stringify(tasks));
+    putInLocalStorage();
 });
 
-
-function showTasks() {
+function displayTasks() {
+    aFaire.innerHTML = "";
+    fait.innerHTML = "";
+    console.log(tasks)
     for (let i = 0; i < tasks.length; i++) {
         let displayTask = `   
-        <li class="currentLi${i}">
-            <div class="texte">
-                ${tasks[i].Text}
-            </div>
-            <div class="valide_delete">
-                <button class="editBtn${i}">
-                    <img src="assets/img/5996831.png" alt="valider">
-                </button>
-                <button class="delBtn${i}">
-                    <img src="assets/img/9153963.png" alt="annuler">
-                </button>
-            </div>
-        </li>
-    `;
+            <li class="currentLi${i}">
+                <div class="texte">
+                    ${tasks[i].Text}
+                </div>
+                <div class="valide_delete">
+                    <button id="btnEdit${i}">
+                        <img src="assets/img/5996831.png" alt="valider">
+                    </button>
+                    <button id="btnDelete${i} ">
+                        <img src="assets/img/9153963.png" alt="annuler">
+                    </button>
+                </div>
+            </li>
+        `;
         if (tasks[i].statement === "undo") {
             aFaire.innerHTML += displayTask;
         } else {
             fait.innerHTML += displayTask;
         }
+        const btnEdit = document.querySelector("#btnEdit" + i);
+        const btnDel = document.querySelector("#btnDelete" + i);
+
+        btnEdit.addEventListener("click", function (event) {
+            if ((tasks[i].statement) === "undo") {
+                tasks[i].statement = "do";
+            }
+            else {
+                tasks[i].statement = "undo";
+            }
+            putInLocalStorage();
+        })
+
+        btnDel.addEventListener('click', function () {
+            tasks.splice(i, 1)
+            putInLocalStorage()
+        })
+
+
+
+
     }
 }
-
-
-
-
-
-
-
-
 
