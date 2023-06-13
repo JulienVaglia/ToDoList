1e874dbcdeb0472a77d406aa26686b4e473984b2
LaurisHebert <lrshebert@gmail.com>
2023-06-13 09:15:48 +0200

    no message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,30 @@
 const d = document;
 const addLine = d.querySelector("#addLine");
-
 const btnUpdateTitle = d.querySelector("#btnUpdateTitle");
 const btnAdd = d.querySelector("#btnAdd");
 const btnCancel = d.querySelector("#btnCancel");
-
 const aFaire = d.querySelector("#aFaire");
 const fait = d.querySelector("#fait");
-
 let tasks;
 
 // récupération de l'ancien localStorage
 if (localStorage.getItem("task") !== null) {
     tasks = JSON.parse(localStorage.getItem("task"));
-    displayTasks()
-}else {
+    displayTasks();
+} else {
     tasks = [];
 }
 
-
 function putInLocalStorage() {
     localStorage.setItem("task", JSON.stringify(tasks));
-    displayTasks()
+    displayTasks();
 }
 
-btnAdd.addEventListener("click", e=>{
+btnAdd.addEventListener("click", e => {
     let task = {
         Text: addLine.value,
         statement: "undo"
-    }
+    };
     tasks.push(task);
     putInLocalStorage();
 });
@@ -40,47 +36,54 @@ function displayTasks() {
     for (let i = 0; i < tasks.length; i++) {
         let displayTask = `   
             <li class="currentLi${i}">
-                <div class="texte" id="textTask${i}">
+                <div id="textTask${i}" class="textTask">
                     ${tasks[i].Text}
                 </div>
                 <div class="valide_delete">
-                    <button id="btnEdit${i}">
+                    <button class="btnEdit" data-index="${i}">
                         <img src="assets/img/5996831.png" alt="valider">
                     </button>
-                    <button id="btnDelete${i}">
+                    <button class="btnDelete" data-index="${i}">
                         <img src="assets/img/9153963.png" alt="annuler">
                     </button>
                 </div>
             </li>
         `;
-
         if (tasks[i].statement === "undo") {
             aFaire.innerHTML += displayTask;
         } else {
             fait.innerHTML += displayTask;
         }
-
-        document.querySelector("#btnEdit" + i).addEventListener("click", e=> {
-
-        })
-
-        document.querySelector("#btnDelete" + i).addEventListener('click', e=> {
-            tasks.splice(i, 1)
-            putInLocalStorage()
-        })
-
-        document.querySelector("#textTask" + 1).addEventListener('click', e=> {
-            console.log(tasks[i].Text)
-
-            if ((tasks[i].statement) === "undo") {
-                tasks[i].statement = "do";
-            }
-            else {
-                tasks[i].statement = "undo";
-            }
-            putInLocalStorage();
-        })
-
     }
-}
+    const btnEditItems = document.querySelectorAll(".btnEdit");
+    btnEditItems.forEach(btn => {
+        btn.addEventListener("click", e => {
+            const index = e.target.dataset.index;
+            // Gérer l'édition de la tâche avec l'indice `index`
+        });
+    });
 
+    const btnDeleteItems = document.querySelectorAll(".btnDelete");
+    btnDeleteItems.forEach(btn => {
+        btn.addEventListener("click", e => {
+            const index = e.target.dataset.index;
+            tasks.splice(index, 1);
+            putInLocalStorage();
+        });
+    });
+
+    const taskItems = document.querySelectorAll(".textTask");
+    taskItems.forEach(item => {
+        item.addEventListener("click", e => {
+            const index = e.target.id.replace("textTask", "");
+            console.log(tasks[index].Text);
+
+            if (tasks[index].statement === "undo") {
+                tasks[index].statement = "do";
+            } else {
+                tasks[index].statement = "undo";
+            }
+            putInLocalStorage})
+
+    })
+}
